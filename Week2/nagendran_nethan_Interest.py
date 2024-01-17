b0195c5f7518e58be351f0486b3aa316d5f43e05
nethann <nethan.nagendran@gmail.com>
2024-01-17 11:13:39 -0500

    updated

--- Week2/nagendran_nethan_Interest.py
+++ Week2/nagendran_nethan_Interest.py
@@ -3,6 +3,7 @@
 Principal = int(input("Please enter the loan principal: "))
 Term = int(input("Please enter the laon term in months: "))
 Rate = float(input("Please enter the annual interest rate of the loan in decimal: "))
+amount = Principal*(1+Rate/12)**Term-Principal
 
 #printing output
-print(f'Amount of interest in this loan:{Principal*(1+Rate/12)**Term-Principal}')
+print(f'Amount of interest in this loan: {amount}')
