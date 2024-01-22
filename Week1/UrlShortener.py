1574784f1de91778ebae419083a39ff861ee61b5
nethann <nethan.nagendran@gmail.com>
2024-01-22 13:24:02 -0500

    updated

---
+++ Week1/UrlShortener.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import ttk
+import pyshorteners
+
+root = Tk()
+root.geometry('400x150')
+root.title("URL shortener")
+root.resizable(FALSE, FALSE)
+
+# Creating a frame to hold the elements
+frame = Frame(root)
+frame.pack()
+
+
+entry = ttk.Entry(frame)
+entry.grid(row=0, column=0, padx=0, pady=5)
+
+label = ttk.Label(frame, text='Nethan Nagendran Productions!')
+label.grid(row=1, column=0, pady=10)
+
+def get_entry_value():
+    # getting value from entry input
+    value = entry.get()
+
+    if value.startswith("https://"):
+        # generating short URl
+        long_url = value
+        type_tiny = pyshorteners.Shortener()
+        short_url = type_tiny.tinyurl.short(long_url)
+
+        # printing out val
+        print(f'Link being generated: {short_url}')
+
+        # Copying to clipboard
+        root.clipboard_clear()
+        root.clipboard_append(short_url)
+
+        label.config(text=f'{short_url}\Has been copied to your clipboard!')
+
+    else:
+        label.config(text="Please make sure you add a VALID http link!")
+
+generate_URL = ttk.Button(frame, text="Generate Short Url", command=get_entry_value)
+generate_URL.grid(row=2, column=0, pady=5)
+
+root.mainloop()
