f360c20a55200ca2ee5e752220fb52083ac45f18
nethann <nethan.nagendran@gmail.com>
2024-03-01 23:23:04 -0500

    updated

--- Week8/nethan_nagendran_guess_my_number.py
+++ Week8/nethan_nagendran_guess_my_number.py
@@ -1,18 +1,21 @@
 import random 
 
-random_val = random.randint(1,100)
-times = 10
+def random_guesser(): 
+        
+    random_val = random.randint(1,100)
+    times = 10
 
-for i in range(0, times): 
-    guess_input = int(input(f'Guess {i + 1}: '))
+    for i in range(0, times): 
+        guess_input = int(input(f'Guess {i + 1}: '))
 
-    if guess_input > random_val: 
-        print("Your guess is greater than my number. Try again")
+        if guess_input > random_val: 
+            print("Your guess is greater than my number. Try again")
 
-    if guess_input < random_val: 
-        print("Your guess is less than my number. Try again")
+        if guess_input < random_val: 
+            print("Your guess is less than my number. Try again")
 
-    if guess_input == random_val: 
-        print(f'Your correctly guessed my random number in {i + 1} times')
-        break
+        if guess_input == random_val: 
+            print(f'Your correctly guessed my random number in {i + 1} times')
+            break
 
+random_guesser()
