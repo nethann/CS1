153a416564000e04b0ac9f600cb8647f3d211ac1
nethann <nethan.nagendran@gmail.com>
2024-03-19 11:30:56 -0400

    updated

---
+++ nethan_nagendran_manage_student_grades.py
@@ -0,0 +1,17 @@
+def manage_student_grades(student_grades, **args):
+    for class_student, class_grade in args.items():
+        if class_grade == None:  
+            student_grades.pop(class_student)
+        else:  
+            student_grades[class_student] = class_grade
+    return student_grades
+
+# Example usage
+student_grades = {'Alice': 85, 'Bob': 72, 'Charlie': 90}
+updated_grades = manage_student_grades(student_grades, David=78, Emily=93)
+updated_grades = manage_student_grades(updated_grades, Bob=80)
+updated_grades = manage_student_grades(updated_grades, Charlie=None)
+print(updated_grades)
+
+
+# {'Alice': 85, 'bob': 80, 'David': 78, 'Emily': 93}
