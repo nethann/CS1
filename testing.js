6581ce95775f9d43f67135f2c3292393c8afe30a
nethann <nethan.nagendran@gmail.com>
2024-03-26 10:46:07 -0400

    updated

---
+++ testing.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return functions
+};
+
+console.log(d1 = compose(2))
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
