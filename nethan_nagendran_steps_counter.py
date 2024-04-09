0965a690503ed114c9fc1d30a2b03b56e789b099
Nethan Nagendran <96995586+nethann@users.noreply.github.com>
2024-04-09 11:54:42 -0400

    Update nethan_nagendran_steps_counter.py

--- nethan_nagendran_steps_counter.py
+++ nethan_nagendran_steps_counter.py
@@ -3,7 +3,6 @@ class NegativeVal(Exception):
     self.message = message
     Exception.__init__(self, self.message)
 
-    
 def steps_to_miles(num_steps):
   try: 
     person_steps = int(num_steps)
@@ -11,17 +10,17 @@ def steps_to_miles(num_steps):
       raise NegativeVal
 
     else:
-      miles = person_steps / 2000
-      return miles
+      miles_counter = person_steps / 2000
+      return miles_counter
 
   except ValueError: 
     raise ValueError("Exception: Non-numeric value Entered")
 
 def main():
   try:
-      steps = input("Enter the number of steps: ")
-      miles = steps_to_miles(steps)
-      print(f'Miles walked: {miles:.2f}')
+      steps = input("Enter the # of steps: ")
+      miles_counter = steps_to_miles(steps)
+      print(f'Miles walked: {miles_counter:.2f}')
   except (ValueError, NegativeVal) as caught_error:
       print(caught_error)
 
